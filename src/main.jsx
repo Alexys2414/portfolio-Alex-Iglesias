fa8cbe8d01bee4b4a38e434b3e30c3e6403a09e1
Alexys2414 <123082404+Alexys2414@users.noreply.github.com>
2025-02-11 01:34:55 +0100

    Primera Personalizacion

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {FeedbacksProvider} from "/src/providers/FeedbacksProvider"
 import {WindowProvider} from "/src/providers/WindowProvider"
 import App from "/src/components/App.jsx"
 import Preloader from "/src/components/Preloader.jsx"
+import { Analytics } from "@vercel/analytics/react"
 
 const AppProviders = ({ children }) => (
     <DataProvider>
